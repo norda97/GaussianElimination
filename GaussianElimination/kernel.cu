69552d918b6077bbb53128f1594e5aef555bc360
Adrian <adde--97@hotmail.com>
2019-12-05 18:52:38 +0100

    For review

--- GaussianElimination/kernel.cu
+++ GaussianElimination/kernel.cu
@@ -19,7 +19,6 @@ bool		g_print;				/* Print switch	*/
 bool		g_compare;				/* Compare switch*/
 matrix		g_A;					/* Matrix A					- Sequential implementation	*/
 double		g_b[MAX_SIZE];			/* Vector b					- Sequential implementation */
-double		g_y[MAX_SIZE];			/* Vector y					- Sequential implementation */
 int			g_threads;				/* Threads Requested		- Parallel implementation */
 matrix		g_Matrix;				/* Matrix					- Parallel implementation */
 double		g_result[MAX_SIZE];		/* Result Vector			- Parallel implementation */
@@ -27,7 +26,7 @@ bool		g_parallel;				/* Determens Implementation	- Parallel implementation */
 /* forward declarations */
 void work(void);
 void Init_Matrix(void);
-void Print_Matrix(matrix Matrix, double vecB[], double vecY[]);
+void Print_Matrix(matrix Matrix, double vec[]);
 void Init_Default(void);
 void Read_Options(int, char**);
 void compareImplementations();
@@ -134,12 +133,12 @@ int main(int argc, char* argv[])
 	else if (g_parallel) {
 		gaussParallel();
 		if (g_print == 1)
-			Print_Matrix(g_Matrix, g_result, nullptr);
+			Print_Matrix(g_Matrix, g_result);
 	}
 	else {
 		work();
 		if (g_print == 1)
-			Print_Matrix(g_A, g_b, g_y);
+			Print_Matrix(g_A, g_b);
 	}
 
 	// Compare sequential and parallel result
@@ -158,35 +157,37 @@ int main(int argc, char* argv[])
     return 0;
 }
 
-void compareImplementations() {
+void compareImplementations() 
+{
+
+	if (g_print == 1) {
+		printf("SERIAL OUTPUT:\n");
+		Print_Matrix(nullptr, g_b);
+		printf("PARALLEL OUTPUT:\n");
+		Print_Matrix(nullptr, g_result);
+	}
+
 	bool different = false;
-	const double epsilon = 0.00001;
+	const double epsilon = 0.000001;
 	for (int i = 0; i < g_N; i++) {
-		if (fabsf(g_result[i] - g_y[i]) > epsilon) {
+		if (fabsf(g_result[i] - g_b[i]) > epsilon) {
 			different = true;
 		}
 	}
 	if (different)
-		printf("Compare: Parallel implementation differs!\n\n");
+		printf("Compare: Parallel implementation differs with a precision of %f!\n\n", epsilon);
 	else
-		printf("Compare: Results are identical!\n\n");
-
-	if (g_print == 1) {
-		printf("SERIAL OUTPUT:\n");
-		Print_Matrix(g_A, g_b, g_y);
-		printf("PARALLEL OUTPUT:\n");
-		Print_Matrix(g_Matrix, g_result, nullptr);
-	}
+		printf("Compare: Results are identical with a precision of atleast %f!\n\n", epsilon);
 }
 
 void Init_Default() {
-	g_N = 3;
+	g_N = 6;
 	g_threads = 1;
-	g_init = "rand";
+	g_init = "fast";
 	g_maxnum = 18.0;
-	g_print = true;
-	g_compare = true;
-	g_parallel = true;
+	g_print = false;
+	g_compare = false;
+	g_parallel = false;
 }
 
 cudaError_t gaussParallel()
@@ -203,9 +204,6 @@ cudaError_t gaussParallel()
 	int matrixAndResultSize = (g_N + 1);
 	cudaStatus = cudaMalloc((void**)&d_Matrix, g_N * matrixAndResultSize * sizeof(double));
 	handleError(cudaStatus, __LINE__);
-	/*
-	cudaStatus = cudaMalloc((void**)&d_VecResult, g_N * sizeof(double));
-	handleError(cudaStatus, __LINE__);*/
 
 	// Write length of Matrix and the maximum number of threads that will be utilized into constant memory
 	cudaStatus = cudaMemcpyToSymbol(d_size, &g_N, sizeof(int), 0, cudaMemcpyHostToDevice);
@@ -246,21 +244,16 @@ cudaError_t gaussParallel()
     cudaStatus = cudaGetLastError();
 	handleError(cudaStatus, __LINE__);
 
-    
-    // cudaDeviceSynchronize waits for the kernel to finish, and returns
-    // any errors encountered during the launch.
+    // Info about errors encountered during the launch.
     cudaStatus = cudaDeviceSynchronize();
     if (cudaStatus != cudaSuccess) {
         fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching gaussElim!\n", cudaStatus);
         goto Error;
     }
 
-	// Copy Matrix and Vector from device buffer to Host.
+	// Copy Vector from device buffer to Host. ( Matrix is not needed as result is aleady ready
 	for (int i = 0; i < g_N; i++) {
 		int stride = i * (g_N + 1);
-		cudaStatus = cudaMemcpy(g_Matrix[i], (d_Matrix + stride), g_N * sizeof(double), cudaMemcpyDeviceToHost);
-		handleError(cudaStatus, __LINE__);
-
 		// Copy Result Vector to Host
 		cudaStatus = cudaMemcpy(&g_result[i], (d_Matrix + g_N + stride), sizeof(double), cudaMemcpyDeviceToHost);
 		handleError(cudaStatus, __LINE__);
@@ -285,24 +278,26 @@ void Read_Options(int argc, char** argv) {
 				g_N = atoi(*++argv);
 				break;
 			case 'h':
-				printf("\nUsage: sor [-n problemsize]\n");
-				printf("           [-d] show default values \n");
-				printf("           [-h] help \n");
-				printf("           [-i init_type] fast/rand \n");
-				printf("           [-m maxnum] max random no \n");
-				printf("           [-p g_print] 0/1 \n");
-				printf("           [-v g_parallel] 0/1 \n");
-				printf("           [-c g_compare] 0/1 \n");
-				printf("           [-t g_threads] Number of threads wanted \n");
+				printf("\nUsage: \n");
+				printf("           [-d] Show default values \n");
+				printf("           [-h] Help \n");
+				printf("           [-n] Matrix size NxN                                    - Input: Interger value greater than 0 \n");
+				printf("           [-m] Maximum values for generation of random matrix	   - Input: Interger value greater than 0 \n");
+				printf("           [-t] Number of threads wanted                           - Input: Interger value greater than 0 \n");
+				printf("           [-i] Generate predetermined matrix or random matrix     - Input: fast/rand \n");
+				printf("           [-v] Run parallel implementation                        - Input: 0/1 \n");
+				printf("           [-p] Print result vectors                               - Input: 0/1 \n");
+				printf("           [-c] Compare results of host and device implementation  - Input: 0/1 \n");
 				exit(0);
 				break;
 			case 'd':
-				printf("\nDefault:  g_N(Matrix Size)			= %d ", g_N);
-				printf("\n          g_init(Initialize type)		= %s", g_init);
-				printf("\n          maxnum(Maximun Number)		= %d", g_maxnum);
-				printf("\n          g_threads					= %d ",(int)g_threads);
-				printf("\n          g_print(Print result)		= %d", (int)g_print);
-				printf("\n          g_compare(Print Compare)	= %d \n\n", (int)g_compare);
+				printf("\nDefault:  g_N(Matrix Size)            = %d ", g_N);
+				printf("\n          g_init(Initialize type)     = %s", g_init);
+				printf("\n          maxnum(Maximun Number)      = %d", g_maxnum);
+				printf("\n          g_threads(Threads)          = %d", (int)g_threads);
+				printf("\n          g_print(Print result)       = %d", (int)g_print);
+				printf("\n          g_compare(Print Compare)    = %d", (int)g_compare);
+				printf("\n          g_parallel(Parallel)        = %d\n\n", (int)g_parallel);
 				exit(0);
 				break;
 			case 'i':
@@ -340,21 +335,31 @@ void work(void)
 {
 	int i, j, k;
 
-	/* Gaussian elimination algorithm, Algo 8.4 from Grama */
 	for (k = 0; k < g_N; k++) { /* Outer loop */
+		double factor = g_A[k][k];
 		for (j = k + 1; j < g_N; j++) {
-			g_A[k][j] = g_A[k][j] / g_A[k][k]; /* Division step */
+			g_A[k][j] = g_A[k][j] / factor; /* Division step */
 		}
-		g_y[k] = g_b[k] / g_A[k][k];
+		g_b[k] = g_b[k] / factor;
 		g_A[k][k] = 1.0;
 
-		for (i = k + 1; i < g_N; i++) {
+		for (i = k + 1; i < g_N+1; i++) {
+			factor = g_A[i][k];
 			for (j = k + 1; j < g_N; j++) {
-				g_A[i][j] = g_A[i][j] - g_A[i][k] * g_A[k][j]; /* Elimination step */
+				g_A[i][j] = g_A[i][j] - factor * g_A[k][j]; /* Forward Elimination step */
 
 			}
-			g_b[i] = g_b[i] - g_A[i][k] * g_y[k];
+			g_b[i] = g_b[i] - factor * g_b[k];
 			g_A[i][k] = 0.0;
+
+			for (j = 0; j < k; j++) {
+				// Per Col
+				factor = g_A[j][k];
+				for (int h = k; h < g_N; h++) {
+					g_A[j][h] = g_A[j][h] - factor * g_A[k][h]; /* Backward Elimination step */
+				}
+				g_b[j] = g_b[j] - factor * g_b[k];
+			}
 		}
 	}	
 }
@@ -367,10 +372,6 @@ void Init_Matrix() {
 		printf("Initialize type		= %s \n", g_init);
 		printf("Threads				= %d \n", g_threads);
 		printf("Initializing matrix...\n\n");
-		if(g_parallel)
-			printf("Parallel Implementation:\n");
-		else
-			printf("Serial Implementation:\n");
 	}
 	if (strcmp(g_init, "rand") == 0) {
 		for (i = 0; i < g_N; i++) {
@@ -397,44 +398,34 @@ void Init_Matrix() {
 		}
 	}
 
-	/* Initialize vectors b and y */
+	/* Initialize result vector */
 	for (i = 0; i < g_N; i++) {
 		g_b[i] = 2.0;
 		g_result[i] = g_b[i];
-		g_y[i] = 1.0;
 	}
 
 	if (g_print == 1)
-		Print_Matrix(g_A, g_b, g_y);
+		Print_Matrix(g_A, g_b);
 }
 
-void Print_Matrix(matrix Matrix, double vecB[], double vecY[]) {
+void Print_Matrix(matrix Matrix, double vec[]) {
 	int i, j;
 
-	printf("Matrix A:\n");
-	for (i = 0; i < g_N; i++) {
-		printf("[");
-		for (j = 0; j < g_N; j++)
-			printf(" %5.2f,", Matrix[i][j]);
-		printf("]\n");
+	// Print Matrix with results
+	if (Matrix != nullptr) {
+		printf("Matrix A:\n");
+		for (i = 0; i < g_N; i++) {
+			printf("[");
+			for (j = 0; j < g_N; j++)
+				printf(" %5.2f,", Matrix[i][j]);
+			printf("]\n");
+		}
 	}
-	if (vecY != nullptr) {
-		printf("Vector b:\n[");
-		for (j = 0; j < g_N; j++)
-			printf(" %5.2f,", vecB[j]);
-		printf("]\n");
+	// Print Vector with results
+	printf("Vector b:\n[");
+	for (j = 0; j < g_N; j++)
+		printf(" %5.2f,", vec[j]);
+	printf("]\n");
 
-		printf("Vector y:\n[");
-		for (j = 0; j < g_N; j++)
-			printf(" %5.2f,", vecY[j]);
-		printf("]\n");
-	}
-	else {
-		printf("Vector result:\n[");
-		for (j = 0; j < g_N; j++)
-			printf(" %5.2f,", vecB[j]);
-		printf("]\n");
-	}
-	
 	printf("\n\n");
 }
